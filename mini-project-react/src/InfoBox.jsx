c9b16d590673923ff535be3265b5d8b504e8b11c
Mohd Talha <talhalko1@gmail.com>
2025-02-22 14:00:05 +0530

    Initial commit

---
+++ mini-project-react/src/InfoBox.jsx
@@ -0,0 +1,46 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import CycloneIcon from '@mui/icons-material/Cyclone';
+
+export default function InfoBox({info}) {
+    const INIT_URL = "https://plus.unsplash.com/premium_photo-1714923303591-3b262dd1864f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZHVzdHklMjBza3l8ZW58MHx8MHx8fDA%3D";
+
+    const HOT_URL = "https://images.unsplash.com/photo-1447601932606-2b63e2e64331?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    const COLD_URL = "https://plus.unsplash.com/premium_photo-1670493556860-13e006e6faa4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8d2ludGVyfGVufDB8fDB8fHww";
+    const RAIN_URL = "https://images.unsplash.com/photo-1503435824048-a799a3a84bf7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFpbnxlbnwwfHwwfHx8MA%3D%3D";
+    const SPRING_URL = "https://images.unsplash.com/photo-1712325910182-fb465c784fe6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3ByaW5nJTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+
+    return (
+        <div className="InfoBox">
+            <div className='cardContainer'>
+            <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={info.humidity > 80 ? RAIN_URL : info.temp > 15 ? SPRING_URL : info.temp > 25 ? HOT_URL : info.temp < 15 ? COLD_URL : INIT_URL}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {info.city} {info.humidity > 80 ? <ThunderstormIcon/> : info.temp > 15 ? <WbSunnyIcon/> : info.temp > 25 ? <WbSunnyIcon/>  : info.temp < 15 ? <AcUnitIcon/> : <CycloneIcon/> }
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
+        <p>Temperature = {info.temp}&deg;C</p>
+        <p>Humidity = {info.humidity}</p>
+        <p>Min Temp = {info.tempMin}&deg;C</p>
+        <p>Max Temp = {info.tempMax}&deg;C</p>
+        <p>
+            The weather can be described as <i>{info.weather}</i> and feels like {info.feelsLike}&deg;C
+        </p>
+        </Typography>
+      </CardContent>
+    </Card>
+    </div>
+        </div>
+        )
+}
